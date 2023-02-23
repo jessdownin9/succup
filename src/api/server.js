816efe9f36279fbe4e2bbc88cedff8e2fcb9aa9f
Jessica Downing <jessicalaurendowning@gmail.com>
2023-02-23 10:19:33 -0700

    Remove homepage from package.json and add get to / route

--- src/api/server.js
+++ src/api/server.js
@@ -3,13 +3,16 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const { prototype } = require('nodemailer/lib/xoauth2');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+
+app.get('/', (req, res) => {
+  res.send('Hello from the root application URL');
+});
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
