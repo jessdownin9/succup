b7251c1454f191f83abff31cab3517a4c50da507
Jessica Downing <jessicalaurendowning@gmail.com>
2023-03-11 14:04:04 -0700

    Remove public from express static line

--- src/api/server.js
+++ src/api/server.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "..", "build")));
     console.log(path.join(__dirname, "..", "build"));
-    app.use(express.static("public"));  
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "..", "build", "index.html"));
         console.log('this one: ', path.join(__dirname, "..", "build", "index.html"));
