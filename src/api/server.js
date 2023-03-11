3d868821bc98a7fbeb7ec3c080d64f29cb4304f5
Jessica Downing <jessicalaurendowning@gmail.com>
2023-03-11 13:41:54 -0700

    Change express static build in server file

--- src/api/server.js
+++ src/api/server.js
@@ -7,8 +7,12 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') app.use(express.static(path.join(__dirname, 'build')));
-console.log(path.join(__dirname, 'build'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+}
 
 const contactRouter = require('./contactRouter');
 app.use("/contact", contactRouter);
