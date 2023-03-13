cc8442698e51522df8c186642ede1e5212047868
Jessica Downing <jessicalaurendowning@gmail.com>
2023-03-13 12:59:26 -0600

    Try removing rows/values from db query result

--- src/api/productsRouter.js
+++ src/api/productsRouter.js
@@ -5,7 +5,7 @@ const db = require('../db/index');
 productsRouter.get('/', (req, res) => {
     db.query('SELECT * FROM products', (err, result) => {
         if (err) console.log(err);
-        res.send(result.values);
+        res.send(result);
     })
 });
 
