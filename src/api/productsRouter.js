c8767bd372efd89497fcae1c1fe9e30750d17c7f
Jessica Downing <jessicalaurendowning@gmail.com>
2023-03-13 10:02:12 -0600

    Try adding console log to productsRouter

--- src/api/productsRouter.js
+++ src/api/productsRouter.js
@@ -5,6 +5,7 @@ const db = require('../db/index');
 productsRouter.get('/', (req, res) => {
     db.query('SELECT * FROM products', (err, result) => {
         if (err) return next(err);
+        console.log(result.rows);
         res.send(result.rows);
     })
 });
